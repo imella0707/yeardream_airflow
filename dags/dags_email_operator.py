6ed3844c7a6ff66e325e9344fa2cffd5fdfb8842
imella0707 <imella0707@gmail.com>
2024-06-17 10:11:12 +0900

    ymal insert

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -10,7 +10,7 @@ with DAG(
 ) as dag:
     send_email_task = EmailOperator(
         task_id='send_email_task',
-        to='gniezno@naver.com',
+        to='imella0707@gmail.com',
         subject='Airflow 성공메일',
         html_content='Airflow 작업이 완료되었습니다'
     )
