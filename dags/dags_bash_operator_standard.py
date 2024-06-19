c04cac466c75ff76652c556d4b2c79a9bd2f9ac2
imella0707 <imella0707@gmail.com>
2024-06-20 01:54:57 +0900

    standard modification upload

--- dags/dags_bash_operator_standard.py
+++ dags/dags_bash_operator_standard.py
@@ -2,23 +2,24 @@ from airflow.decorators import dag
 import pendulum
 from airflow.operators.bash import BashOperator
 
-@dag(dag_id='dags_bash_operator_decorator',
-     schedule="0 9 * * 1,5",
-     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
-     catchup=False,
-     tags=['homework']
+dag = DAG(
+    dag_id="dags_bash_operator_standard",
+    schedule="0 9 * * 1,5",
+    start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=['homework']
 )
-def dags_bash_operator_decorator():
-    bash_t1 = BashOperator(
-        task_id="bash_t1",
-        bash_command="echo whoami"
-    )
 
-    bash_t2 = BashOperator(
-        task_id="bash_t2",
-        bash_command="echo $HOSTNAME"
-    )
+bash_t1 = BashOperator(
+    task_id="bash_t1",
+    bash_command="echo whoami",
+    dag=dag
+)
 
-    bash_t1 >> bash_t2
+bash_t2 = BashOperator(
+    task_id="bash_t2",
+    bash_command="echo $HOSTNAME",
+    dag=dag
+)
 
-dags_bash_operator_decorator()
+bash_t1 >> bash_t2
